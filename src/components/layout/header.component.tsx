0be26265e4739a4c316a8d3ade841c9105153ce7
Stephania Martheyn <stephaniemartheyn@gmail.com>
2022-12-12 15:56:24 -0500

    parcial terminado

---
+++ src/components/layout/header.component.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import "./header.css";
+
+/**
+ * Header component
+ *
+ * @returns {TSX.Element}
+ */
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <div>
+          <h2>Examen Final de Frontend IV</h2>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Inicio</Link>
+            </li>
+            <li>
+              <Link to="/favorites">Favoritos</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.page.tsx
@@ -0,0 +1,39 @@
+import React, { FC } from 'react'
+import Filters from "../components/characters/filters.component";
+import GridCharacters from "../components/characters/grid-characters.component";
+import Pagination from "../components/pagination/pagination.component";
+import { fetchCharactersThunk } from "../actions/characters.actions";
+import { useDispatch } from "react-redux";
+
+/**
+ * Home page
+ *
+ * @returns {React.ReactElement} TSX element
+ */
+const HomePage: FC = () => {
+  const dispatch = useDispatch();
+
+  /**
+   * Function that removes the filters
+   */
+  const deleteFiltersOnClick = () => {
+    dispatch(fetchCharactersThunk(""));
+  };
+
+  return (
+    <div className="container">
+      <div className="actions">
+        <h3>Catálogo de Personajes</h3>
+        <button className="danger" onClick={deleteFiltersOnClick}>
+          Limpiar filtros
+        </button>
+      </div>
+      <Filters />
+      <Pagination />
+      <GridCharacters />
+      <Pagination />
+    </div>
+  );
+};
+
+export default HomePage;
